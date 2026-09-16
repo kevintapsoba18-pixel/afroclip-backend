358a4a7cfd77b91ce8120e912b5e605c16a80040
kevintapsoba18-pixel <kevintapsoba18@gmail.com>
2026-09-16 22:20:39 +0000

    Add '--js-runtimes' argument to args array

--- server.js
+++ server.js
@@ -158,6 +158,7 @@ async function downloadVideo(youtubeUrl, outputPath) {
     '-f', 'bestvideo[height<=720][ext=mp4]+bestaudio[ext=m4a]/best[height<=720][ext=mp4]/best',
     '--merge-output-format', 'mp4',
     '--no-playlist',
+    '--js-runtimes', 'node',
     '-o', outputPath
   ];
   if (COOKIES_PATH) args.push('--cookies', COOKIES_PATH);
